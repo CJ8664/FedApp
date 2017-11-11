8a79a26cb886afc78c994503643ecc0852236c40
Chirag Jain <csjain@ncsu.edu>
2017-11-10 22:51:37 -0500

    Revert to normal jQuery app from react

---
+++ public/js/fire.js
@@ -0,0 +1,9 @@
+var config = {
+		apiKey: "AIzaSyCmrV94B42rB-vDB_F0EJ_68dDPa2vN-qI",
+		authDomain: "fedapp-ncsu.firebaseapp.com",
+		databaseURL: "https://fedapp-ncsu.firebaseio.com",
+		projectId: "fedapp-ncsu",
+		storageBucket: "fedapp-ncsu.appspot.com",
+		messagingSenderId: "793785966281"
+};
+var fire = firebase.initializeApp(config);
